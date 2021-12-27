833b30a30dde0e7e8f3714bd9f7bc7edbceffd83
Tomas Zeman <tomaas.zeman@gmail.com>
2021-12-27 23:49:17 +0100

    2015 - Day 8b

--- year2015/day8/part1.py
+++ year2015/day8/part1.py
@@ -2,8 +2,4 @@ from typing import List
 
 
 def run(data: List[str]):
-    result = 0
-    for string in data:
-        decoded_string = bytes(string, 'utf-8').decode('unicode_escape')
-        result += len(string) - (len(decoded_string) - 2)
-    return result
+    return sum([len(string) - (len(bytes(string, 'utf-8').decode('unicode_escape')) - 2) for string in data])
