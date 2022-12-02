48e79a0f1efb4d51c04be8ffbc0a12f69174d2d6
Tomas Zeman <tomaas.zeman@gmail.com>
2022-12-02 18:54:15 +0100

    2022 - Day 2

---
+++ year2022/day2/common.py
@@ -0,0 +1,30 @@
+from enum import Enum
+from __future__ import annotations
+
+
+class Option(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+    def compareWith(self, option: Option):
+        if self.value - option.value in [-1, 2]:
+            return Result.LOSS
+        if self.value == option.value:
+            return Result.DRAW
+        return Result.WIN
+
+    def optionForResult(self, result: Result):
+        if result == Result.DRAW:
+            return self
+        if result == Result.LOSS:
+            value = self.value - 1
+            return Option(value if value > 0 else 3)
+        value = self.value + 1
+        return Option(value if value < 4 else 1)
+
+
+class Result(Enum):
+    LOSS = 0
+    DRAW = 3
+    WIN = 6
---
+++ year2022/day2/part2.py
@@ -0,0 +1,21 @@
+from typing import List
+from enum import Enum
+from year2022.day2.common import Option, Result
+
+
+class Mapping(Enum):
+    A = Option.ROCK
+    B = Option.PAPER
+    C = Option.SCISSORS
+    X = Result.LOSS
+    Y = Result.DRAW
+    Z = Result.WIN
+
+
+def run(data: List[str]):
+    points = 0
+    for line in data:
+        enemy: Option = Mapping[line.split(' ')[0]].value
+        result: Result = Mapping[line.split(' ')[1]].value
+        points += result.value + enemy.optionForResult(result).value
+    return points
