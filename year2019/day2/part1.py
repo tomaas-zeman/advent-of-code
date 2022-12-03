d3d4f52562a8168ac84bb688d7e207b7dac651b4
Tomas Zeman <tomaas.zeman@gmail.com>
2022-12-03 12:08:48 +0100

    2019 - Day 2

---
+++ year2019/day2/part1.py
@@ -0,0 +1,20 @@
+from typing import List
+from common.lists import as_ints
+
+
+def run(data: List[str]):
+    codes = as_ints(data[0].split(","))
+
+    # inputs (task description)
+    codes[1] = 12
+    codes[2] = 2
+
+    i = 0
+    while codes[i] != 99:
+        opcode = codes[i]
+        if opcode == 1:
+            codes[codes[i + 3]] = codes[codes[i + 1]] + codes[codes[i + 2]]
+        if opcode == 2:
+            codes[codes[i + 3]] = codes[codes[i + 1]] * codes[codes[i + 2]]
+        i += 4
+    return codes[0]
---
+++ year2019/day2/part2.py
@@ -0,0 +1,22 @@
+from typing import List
+from common.lists import as_ints
+
+
+def run(data: List[str]):
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+            codes = as_ints(data[0].split(","))
+            codes[1] = noun
+            codes[2] = verb
+
+            i = 0
+            while codes[i] != 99:
+                opcode = codes[i]
+                if opcode == 1:
+                    codes[codes[i + 3]] = codes[codes[i + 1]] + codes[codes[i + 2]]
+                if opcode == 2:
+                    codes[codes[i + 3]] = codes[codes[i + 1]] * codes[codes[i + 2]]
+                i += 4
+
+            if codes[0] == 19690720:
+                return 100 * noun + verb
