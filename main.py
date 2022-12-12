3ebdef0150254695f6abb03ad199fc98c98609d4
Tomas Zeman <tomaas.zeman@gmail.com>
2022-12-12 23:39:15 +0100

    [Draft] 2022 - Day 12

--- main.py
+++ main.py
@@ -27,42 +27,45 @@ def compute_solution(module: str, part: int, data: list[str], raw_data: list[str
         return (import_module(module).run(data, raw_data=raw_data), None)
 
 
-def run_with_file(filename: str):
+def run_with_file(filename: str, part: int):
     run_result = True
-    for part in [1, 2]:
-        input_file = f"{year}/{day}/{filename}"
-        expected_test_solution = None
-        with open(input_file, "r") as file:
-            raw_data = file.readlines()
-            data = [line.strip() for line in raw_data]
-            try:
-                module = f"{year}.{day}.part{part}"
-                solution, expected_test_solution = compute_solution(module, part, data, raw_data)
-            except Exception:
-                solution = "ERR"
-                print(traceback.format_exc())
-                run_result = False
+    input_file = f"{year}/{day}/{filename}"
+    expected_test_solution = None
+    with open(input_file, "r") as file:
+        raw_data = file.readlines()
+        data = [line.strip() for line in raw_data]
+        try:
+            module = f"{year}.{day}.part{part}"
+            solution, expected_test_solution = compute_solution(module, part, data, raw_data)
+        except Exception:
+            solution = "ERR"
+            print(traceback.format_exc())
+            run_result = False
 
-            color = Color.GREEN
-            symbol = "✔"
-            if (
-                solution is None
-                or (expected_test_solution is not None and expected_test_solution != str(solution))
-                or solution == "ERR"
-            ):
-                color = Color.BOLD_RED
-                symbol = "✘"
-                run_result = False
+        color = Color.GREEN
+        symbol = "✔"
+        if (
+            solution is None
+            or (expected_test_solution is not None and expected_test_solution != str(solution))
+            or solution == "ERR"
+        ):
+            color = Color.BOLD_RED
+            symbol = "✘"
+            run_result = False
 
-            print(with_color(f"{symbol} {year} | {day} | part{part:02d} => {solution}", color))
+        print(with_color(f"{symbol} {year} | {day} | part{part:02d} => {solution}", color))
     return run_result
 
 
-# 2021 does not support testdata
-if "2021" in year:
-    run_with_file("data")
-else:
-    print("\nRunning with test data ...\n")
-    if run_with_file("testdata"):
-        print("\nPASSED. Running with real data ...\n")
-        run_with_file("data")
+for part in [1, 2]:
+    # 2021 does not support testdata
+    if "2021" in year:
+        run_with_file("data", part)
+    else:
+        print("\nRunning with test data ...\n")
+        if run_with_file("testdata", part):
+            print("\nPASSED. Running with real data ...\n")
+            run_with_file("data", part)
+        else:
+            break
+        
---
+++ year2022/day12/part2.py
@@ -0,0 +1,2 @@
+def run(data: list[str], raw_data: list[str]):
+    pass
