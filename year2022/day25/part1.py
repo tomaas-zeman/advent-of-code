180e586774dd14efa0e3ca393d5fbf46e7be1992
Tomas Zeman <tomaas.zeman@gmail.com>
2022-12-25 19:40:14 +0100

    2022 - Day 25

---
+++ year2022/day25/part1.py
@@ -0,0 +1,20 @@
+dec_multiplier = {"2": 2, "1": 1, "0": 0, "-": -1, "=": -2}
+snafu_mod = [("0", 0), ("1", 0), ("2", 0), ("=", 1), ("-", 1)]  #  tuple (result, carry)
+
+
+def snafu_to_dec(snafu: str):
+    return sum([dec_multiplier[snafu[::-1][i]] * (5**i) for i in range(len(snafu))])
+
+
+def dec_to_snafu(dec: int):
+    res = ""
+    while dec > 0:
+        mod = dec % 5
+        res += snafu_mod[mod][0]
+        dec //= 5
+        dec += snafu_mod[mod][1]
+    return res[::-1]
+
+
+def run(data: list[str], raw_data: list[str], is_test: bool):
+    return dec_to_snafu(sum([snafu_to_dec(line) for line in data]))
