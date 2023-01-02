123c1b14153ed9f5d161f41a6677ebb39bd6b7d9
Tomas Zeman <tomaas.zeman@gmail.com>
2023-01-02 18:16:36 +0100

    2022 - Day 16 part 1 done

--- year2022/day16/part1.py
+++ year2022/day16/part1.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 import networkx as nx
 import re
-import matplotlib.pyplot as plt
-from itertools import permutations
-import time
+from queue import PriorityQueue
 
 
 class Valve:
@@ -16,27 +14,22 @@ class Valve:
     def __eq__(self, other: Valve):
         return self.id == other.id
 
+    def __lt__(self, other: Valve):
+        return other.flow_rate - self.flow_rate
+
     def __hash__(self):
         return hash(self.id)
 
 
-def remap_by_id(valves: list[Valve]):
+def parse_input(data: list[str]):
+    pattern = re.compile("Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z, ]+)")
+    valves = [
+        Valve(m[1], int(m[2]), [n.strip() for n in m[3].split(",")]) for line in data if (m := pattern.match(line))
+    ]
     valves_by_id = {n.id: n for n in valves}
     for n in valves:
         n.next_valves = [valves_by_id[id] for id in n.next_valve_ids]
     return valves_by_id
-
-
-def parse_input(data: list[str]):
-    pattern = re.compile("Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z, ]+)")
-    return remap_by_id(
-        [Valve(m[1], int(m[2]), [n.strip() for n in m[3].split(",")]) for line in data if (m := pattern.match(line))]
-    )
-
-
-def print_graph(graph: nx.Graph):
-    nx.draw(graph, nx.spring_layout(graph, seed=1), with_labels=True)
-    plt.show()
 
 
 def run(data: list[str], is_test: bool):
@@ -49,32 +42,42 @@ def run(data: list[str], is_test: bool):
 
     distances = nx.floyd_warshall(graph)
     valid_valves = [valve for valve in valves.values() if valve.flow_rate > 0]
+    valve_bits = {valve: bit for bit, valve in enumerate(valid_valves)}
+    all_valves_visited = sum([1 << i for i in range(len(valid_valves))])
 
-    # TODO: can't really work with full permutations -> too many -> have to skip them continually
-    # TODO: try to generate the permutations recursively + memoize all the stuff -> dynamic programming, duh!
-    flows = {}
-    for permutation in permutations(valid_valves):
-        permutation = [valves["AA"]] + list(permutation)
-        time_left = 30
-        flow = 0
+    solutions = []
+    best_flows: dict[int, int] = {}
 
-        for i in range(len(permutation)):
-            try:
-                [src, dest] = permutation[i : i + 2]
-            except ValueError:
-                src = permutation[i]
-                dest = None
+    paths = PriorityQueue[tuple[int, Valve, int, int, int]]()
+    paths.put((0, valves["AA"], 0, 30, 0))
 
-            if src.flow_rate > 0:
-                time_left -= 1
-                flow += src.flow_rate * time_left
+    while not paths.empty():
+        _, current_valve, visited_valves_bitmap, time_left, flow = paths.get()
 
-            if dest is not None:
-                time_left -= distances[src.id][dest.id]
-            else:
-                flows[flow] = permutation
+        unvisited_valves = [v for v in valid_valves if not visited_valves_bitmap & (1 << valve_bits[v])]
+        for next_valve in unvisited_valves:
+            # go to valve
+            new_time_left = time_left - int(distances[current_valve.id][next_valve.id])
 
-            if time_left <= 0:
+            # open it
+            new_time_left = new_time_left - 1
+            new_flow = flow + (new_time_left * next_valve.flow_rate)
+
+            # update state and queue it if possible
+            new_visited_valves_bitmap = visited_valves_bitmap | (1 << valve_bits[next_valve])
+
+            if new_visited_valves_bitmap == all_valves_visited:
+                solutions.append(new_flow)
                 break
 
-    return int(max(flows.keys()))
+            if new_visited_valves_bitmap not in best_flows:
+                best_flows[new_visited_valves_bitmap] = new_flow
+            elif best_flows[new_visited_valves_bitmap] > new_flow:
+                continue                
+
+            if new_time_left >= 0:
+                paths.put((-new_flow, next_valve, new_visited_valves_bitmap, new_time_left, new_flow))
+            else:
+                solutions.append(flow)
+
+    return max(solutions)
