6895ceecb3dc16bc38c45eec6c0d3b08e7fd7680
Tomas Zeman <tomaas.zeman@gmail.com>
2023-01-02 20:46:09 +0100

    [WIP] 2022 - Day 19

---
+++ year2022/day19/common.py
@@ -0,0 +1,79 @@
+import re
+from enum import Enum
+
+
+class Type(Enum):
+    ORE = 0
+    CLAY = 1
+    OBSIDIAN = 2
+    GEODE = 3
+
+    @staticmethod
+    def all():
+        return [Type.ORE, Type.CLAY, Type.OBSIDIAN, Type.GEODE]
+
+
+class Robot:
+    def __init__(self, type: Type, ore: int = 0, clay: int = 0, obsidian: int = 0):
+        self.type = type
+        self.cost = (ore, clay, obsidian, 0)
+        self.production = [1 if type == t else 0 for t in list(Type)]
+
+
+def as_list(robots: list[Robot] | Robot) -> list[Robot]:
+    return robots if isinstance(robots, list) else [robots]
+
+
+class Resources:
+    # resources: tuple[ore, clay, obsidian, geode]
+    def __init__(self, resources: tuple[int, int, int, int] = (0, 0, 0, 0)) -> None:
+        self.resources = resources
+
+    def __getitem__(self, type: Type):
+        return self.resources[type.value]
+
+    def __hash__(self):
+        return hash(self.resources)
+
+    def enough_for(self, robots: list[Robot] | Robot):
+        total_cost = [sum(r) for r in zip(*[r.cost for r in as_list(robots)])]
+        if not total_cost:
+            return True
+        return all([total_cost[i] <= self.resources[i] for i in range(len(total_cost))])
+
+    def add_mined_with(self, robots: list[Robot] | Robot):
+        return Resources(tuple([sum(res) for res in zip(self.resources, *[r.production for r in as_list(robots)])]))
+
+    def pay_for(self, robots: list[Robot] | Robot):
+        return Resources(
+            tuple([sum(x) for x in zip(self.resources, *[map(lambda x: -x, robot.cost) for robot in as_list(robots)])])
+        )
+
+
+class Blueprint:
+    def __init__(self, id: int, ore_robot: Robot, clay_robot: Robot, obsidian_robot: Robot, geode_robot: Robot):
+        self.id = id
+        self.ore_robot = ore_robot
+        self.clay_robot = clay_robot
+        self.obsidian_robot = obsidian_robot
+        self.geode_robot = geode_robot
+
+    def robots(self):
+        return [self.ore_robot, self.clay_robot, self.obsidian_robot, self.geode_robot]
+
+
+def parse_input(data: list[str]):
+    pattern = re.compile(
+        r"Blueprint (\d+): .+ costs (\d+) ore. .+ costs (\d+) ore. .+ costs (\d+) ore and (\d+) clay. .+ costs (\d+) ore and (\d+) obsidian."
+    )
+    return [
+        Blueprint(
+            int(m[1]),
+            Robot(Type.ORE, ore=int(m[2])),
+            Robot(Type.CLAY, ore=int(m[3])),
+            Robot(Type.OBSIDIAN, ore=int(m[4]), clay=int(m[5])),
+            Robot(Type.GEODE, ore=int(m[6]), obsidian=int(m[7])),
+        )
+        for line in data
+        if (m := pattern.match(line))
+    ]
--- year2022/day19/part1.py
+++ year2022/day19/part1.py
@@ -1,2 +1,78 @@
+from __future__ import annotations
+from year2022.day19.common import Resources, Robot, Type, parse_input
+from queue import PriorityQueue
+
+
+class State:
+    def __init__(
+        self,
+        resources: Resources = Resources(),
+        robots: list[Robot] = [],
+        time_left: int = 24,
+    ):
+        self.resources = resources
+        self.robots = robots
+        self.time_left = time_left
+        self.id = (
+            self.resources,
+            self.time_left,
+            tuple([r.type for r in sorted(self.robots, key=lambda r: r.type.value)]),
+        )
+
+    def robots_by_type(self, type: Type):
+        return [robot for robot in self.robots if robot.type == type]
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __str__(self):
+        return f"{self.resources} | {' '.join(r.type.name for r in self.robots)}"
+
+
+def try_to_build_a_robot(state: State, robot: Robot, resource_limits: dict[Type, int]):
+    if state.resources.enough_for(robot) and (
+        resource_limits[robot.type] == 0 or state.resources[robot.type] < resource_limits[robot.type]
+    ):
+        new_resources = state.resources.add_mined_with(state.robots).pay_for(robot)
+        new_state = State(new_resources, state.robots + [robot], state.time_left - 1)
+        return (-robot.type.value, new_state)
+
+
 def run(data: list[str], is_test: bool):
-    return "https://media.giphy.com/media/spfi6nabVuq5y/giphy.gif"
+    blueprints = parse_input(data)
+
+    results: dict[int, int] = {}
+
+    for bp in blueprints:
+        resource_limits = {type: max([robot.cost[type.value] for robot in bp.robots()]) for type in Type.all()}
+
+        states = PriorityQueue[tuple[int, State]]()
+        states.put((0, State(robots=[Robot(Type.ORE)])))
+        cache: dict[State, int] = {}
+
+        while not states.empty():
+            priority, state = states.get()
+
+            if cache.get(state, 0) > state.resources[Type.GEODE]:
+                continue
+            else:
+                cache[state] = state.resources[Type.GEODE]
+
+            if state.time_left == 0:
+                results[bp.id] = state.resources[Type.GEODE]
+                continue
+
+            next_states = [
+                new_state
+                for new_state in [try_to_build_a_robot(state, robot, resource_limits) for robot in bp.robots()]
+                if new_state is not None
+            ]
+
+            if len(next_states) > 0:
+                states.put(next_states[-1])  # the last one has robots with the highest priority
+            else:
+                states.put(
+                    (priority, State(state.resources.add_mined_with(state.robots), state.robots, state.time_left - 1))
+                )
+
+    return sum([id * res for id, res in results.items()])
