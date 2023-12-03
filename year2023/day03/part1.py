5bac78c6e7b151761173b584eeb7379737179fd6
Tomas Zeman <tomaas.zeman@gmail.com>
2023-12-03 17:28:04 +0100

    Tiny naming refactoring again

--- year2023/day03/part1.py
+++ year2023/day03/part1.py
@@ -1,12 +1,14 @@
 from year2023.day03.common import parse, Item
 
 
-def get_adj_items(main: Item, others: list[Item]):
-    return [
-        o for o in others if main.row + 1 >= o.row >= main.row - 1 and main.span[0] - 1 <= o.span[0] <= main.span[1]
-    ]
+def has_adj_special_char(number: Item, specials: list[Item]):
+    return any(
+        s
+        for s in specials
+        if number.row + 1 >= s.row >= number.row - 1 and number.span[0] - 1 <= s.span[0] <= number.span[1]
+    )
 
 
 def run(data: list[str], is_test: bool):
     numbers, specials = parse(data)
-    return sum([int(n.value) for n in numbers if any(get_adj_items(n, specials))])
+    return sum([int(n.value) for n in numbers if has_adj_special_char(n, specials)])
