ad56acd2a9e1df8dea96e16cca3e64b3dfeb37c0
Tomas Zeman <tomaas.zeman@gmail.com>
2023-12-11 16:36:22 +0100

    2023 - Day 11

---
+++ year2023/day11/common.py
@@ -0,0 +1,37 @@
+from itertools import combinations
+
+
+def parse(data: list[str]):
+    galaxies = [
+        (row_index, col_index)
+        for row_index, row in enumerate(data)
+        for col_index, value in enumerate(row)
+        if value == "#"
+    ]
+    empty_row_indexes = [row_index for row_index, row in enumerate(data) if all(c == "." for c in row)]
+    empty_col_indexes = [col_index for col_index in range(len(data[0])) if all([row[col_index] == "." for row in data])]
+
+    return galaxies, empty_row_indexes, empty_col_indexes
+
+
+def empty_space(i: int, j: int, empty_indexes: list[int], expand_factor: int):
+    empty_space = 0
+    for empty_index in empty_indexes:
+        if min(i, j) < empty_index < max(i, j):
+            empty_space += expand_factor - 1
+    return empty_space
+
+
+def sum_of_shortest_paths(data, expand_factor):
+    galaxies, empty_row_indexes, empty_col_indexes = parse(data)
+    pairs = list(combinations(galaxies, 2))
+
+    return sum(
+        [
+            abs(p1[0] - p2[0])
+            + empty_space(p1[0], p2[0], empty_row_indexes, expand_factor)
+            + abs(p1[1] - p2[1])
+            + empty_space(p1[1], p2[1], empty_col_indexes, expand_factor)
+            for p1, p2 in pairs
+        ]
+    )
---
+++ year2023/day11/part1.py
@@ -0,0 +1,5 @@
+from year2023.day11.common import sum_of_shortest_paths
+
+
+def run(data: list[str], is_test: bool):
+    return sum_of_shortest_paths(data, 2)
