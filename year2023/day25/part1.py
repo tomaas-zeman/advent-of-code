c6aee8a7bd2c21e24f8a187562b92d1fd1529610
Tomas Zeman <tomaas.zeman@gmail.com>
2023-12-31 00:30:30 +0100

    Major simplification of 2023/25 by using minimum_cut function

--- year2023/day25/part1.py
+++ year2023/day25/part1.py
@@ -1,20 +1,7 @@
 import re
-from functools import reduce
 from itertools import combinations
-from typing import Callable
 
 import networkx as nx
-
-
-def solve(graph: nx.Graph, generate_edge_combinations: Callable[[nx.Graph], list[list[tuple[str, str]]]]):
-    for edges_to_remove in generate_edge_combinations(graph):
-        [graph.remove_edge(src, dst) for src, dst in edges_to_remove]
-        components = list(nx.connected_components(graph))
-
-        if len(components) > 1:
-            return reduce(lambda acc, comp: acc * len(comp), components, 1)
-        else:
-            [graph.add_edge(src, dst) for src, dst in edges_to_remove]
 
 
 def run(data: list[str], is_test: bool):
@@ -25,11 +12,9 @@ def run(data: list[str], is_test: bool):
     ]:
         graph.add_nodes_from([source] + destinations)
         for destination in destinations:
-            graph.add_edge(source, destination)
+            graph.add_edge(source, destination, capacity=1)
 
-    if is_test:
-        # Slow and complete solution
-        return solve(graph, lambda g: combinations(g.edges, 3))
-
-    # Quick solution based on visualisation: Graph.draw(graph)
-    return solve(graph, lambda _: [[("xnn", "txf"), ("nhg", "jjn"), ("tmc", "lms")]])
+    for node1, node2 in combinations(graph.nodes, 2):
+        cut_size, partition = nx.minimum_cut(graph, node1, node2)
+        if cut_size == 3:
+            return len(partition[0]) * len(partition[1])
