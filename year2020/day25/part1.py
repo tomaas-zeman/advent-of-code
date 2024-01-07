53ac060a48f791b87902ff5a65683d1a91ef7dd2
Tomas Zeman <tomaas.zeman@gmail.com>
2024-01-07 19:50:39 +0100

    2020 - Day 25

--- year2020/day25/part1.py
+++ year2020/day25/part1.py
@@ -1,2 +1,23 @@
+from aocutils import as_ints
+
+
 def run(data: list[str], is_test: bool):
-    pass
+    card_key, door_key = as_ints(data)
+
+    subject_number = 1
+    loop_size = 1
+    while True:
+        subject_number *= 7
+        subject_number %= 20201227
+
+        if card_key == subject_number:
+            break
+
+        loop_size += 1
+
+    encryption_key = 1
+    for i in range(loop_size):
+        encryption_key *= door_key
+        encryption_key %= 20201227
+
+    return encryption_key
