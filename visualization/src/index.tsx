9caaf8405431e4ad1d21f206dd3001fffa295692
Tomas Zeman <tomaas.zeman@gmail.com>
2024-11-19 21:19:45 +0100

    Tailwind skeleton

--- visualization/src/index.tsx
+++ visualization/src/index.tsx
@@ -4,15 +4,16 @@ import { NoVisualization, Visualizer, visualizers } from './visualizers';
 import './index.css';
 
 enum State {
-  IDLE = 'idle',
-  LOADING_DATA = 'loading data ...',
-  DATA_LOADED = 'data loaded',
+  IDLE,
+  LOADING_DATA,
+  DATA_LOADED,
 }
 
 function App() {
   const buffer = useRef<string[]>([]);
   const visualizer = useRef<Visualizer>(NoVisualization);
   const [state, setState] = useState<State>(State.IDLE);
+  const [date, setDate] = useState<string>('');
 
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:3333');
@@ -22,6 +23,7 @@ function App() {
         buffer.current = [];
         visualizer.current = visualizers[data.year][data.day];
         setState(State.LOADING_DATA);
+        setDate(`${data.year} / ${data.day}`);
       } else if (data.stop) {
         setState(State.DATA_LOADED);
       } else {
@@ -34,10 +36,15 @@ function App() {
   const Visualizer = visualizer.current;
 
   return (
-    <>
-      <div>Client state: {state}</div>
+    <div className="container m-20">
+      <h1 className="text-5xl mb-6">Advent of Code Visualization</h1>
+      <h2 className="text-xl italic mb-6">{date}</h2>
+      <hr className="mb-6" />
       {state === State.DATA_LOADED && <Visualizer buffer={buffer.current} />}
-    </>
+      {state !== State.DATA_LOADED && (
+        <div className="text-xl">Waiting for visualization data ...</div>
+      )}
+    </div>
   );
 }
 
---
+++ visualization/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.tsx", "./styles/index.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
