267b46c5ae16a78239394e8187bf0efe61be8a14
Tomas Zeman <tomaas.zeman@gmail.com>
2024-12-06 18:11:58 +0100

    Rework 2024/06 to use worker threads and run in parallel

--- year2024/day06/part2.ts
+++ year2024/day06/part2.ts
@@ -1,50 +1,35 @@
 import { Config } from '../..';
-import { Tile, MOVEMENT_CHANGES, parse } from './common';
+import { sum } from '../../aocutils';
+import { parse } from './common';
+import chunk from 'lodash/chunk';
+import { Worker } from 'worker_threads';
+
+const WORKER_THREADS = 10;
 
 export async function run(data: string[], config: Config): Promise<string | number> {
-  const [startingMap, startingPosition] = parse(data);
-
-  let goodObstacles = 0;
-
-  for (const [obstacleRow, obstacleCol] of startingMap.positions()) {
-    const map = startingMap.clone();
-
-    // Not necessary but it improves performance by 25%
-    if (map.get(obstacleRow, obstacleCol) === Tile.OBSTACLE) {
-      continue;
-    }
-    map.set(obstacleRow, obstacleCol, Tile.OBSTACLE);
-    
-    let [row, col] = startingPosition;
-    let movement = 0;
-    const moves = new Set();
-
-    while (true) {
-      map.set(row, col, Tile.VISITED);
-
-      const nextRow = row + MOVEMENT_CHANGES[movement][0];
-      const nextCol = col + MOVEMENT_CHANGES[movement][1];
-      if (nextRow < 0 || nextRow >= map.rows || nextCol < 0 || nextCol >= map.cols) {
-        break;
-      }
-
-      const move = `${row}:${col}->${nextRow},${nextCol}`;
-      if (moves.has(move)) {
-        goodObstacles++;
-        break;
-      }
-      moves.add(move);
-
-      if (map.get(nextRow, nextCol) === Tile.OBSTACLE) {
-        movement = (movement + 1) % 4;
-      } else {
-        row = nextRow;
-        col = nextCol;
-      }
-    }
+  if (config.isTest) {
+    return '';
   }
 
-  return goodObstacles;
+  const [startingMap, startingPosition] = parse(data);
+
+  const workers: Promise<number>[] = [];
+  const chunks = chunk(
+    [...startingMap.positions()],
+    (startingMap.cols * startingMap.cols) / WORKER_THREADS,
+  );
+
+  for (const positions of chunks) {
+    const workerData = { startingMap, positions, startingPosition };
+    workers.push(
+      new Promise((resolve) => {
+        const worker = new Worker('./year2024/day06/worker.js', { workerData });
+        worker.on('message', resolve);
+      }),
+    );
+  }
+
+  return sum(await Promise.all(workers));
 }
 
-export const testResult = 6;
+export const testResult = '';
---
+++ year2024/day06/worker.js
@@ -0,0 +1,52 @@
+import { parentPort, workerData } from 'worker_threads';
+
+const Tile = {
+  OBSTACLE: '#',
+  VISITED: 'X',
+};
+
+const MOVEMENT_CHANGES = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+];
+
+const { startingMap, positions, startingPosition } = workerData;
+
+let goodObstacles = 0;
+
+for (const [obstacleRow, obstacleCol] of positions) {
+  const map = startingMap.data.map((row) => row.slice());
+  map[obstacleRow][obstacleCol] = Tile.OBSTACLE;
+
+  let [row, col] = startingPosition;
+  let movement = 0;
+  const moves = new Set();
+
+  while (true) {
+    map[row][col] = Tile.VISITED;
+
+    const nextRow = row + MOVEMENT_CHANGES[movement][0];
+    const nextCol = col + MOVEMENT_CHANGES[movement][1];
+    if (nextRow < 0 || nextRow >= map.length || nextCol < 0 || nextCol >= map[0].length) {
+      break;
+    }
+
+    const move = `${row}:${col}->${nextRow},${nextCol}`;
+    if (moves.has(move)) {
+      goodObstacles++;
+      break;
+    }
+    moves.add(move);
+
+    if (map[nextRow][nextCol] === Tile.OBSTACLE) {
+      movement = (movement + 1) % 4;
+    } else {
+      row = nextRow;
+      col = nextCol;
+    }
+  }
+}
+
+parentPort.postMessage(goodObstacles);
