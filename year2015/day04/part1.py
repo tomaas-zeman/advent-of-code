8062521636420b8dbe8bead80607257c1fe0284e
Tomas Zeman <tomaas.zeman@gmail.com>
2025-01-05 19:54:38 +0100

    2015 - Day 04

---
+++ year2015/day04/part1.py
@@ -0,0 +1,8 @@
+from hashlib import md5
+
+
+def run(data: list[str], is_test: bool):
+    for n in range(0, 1_000_000):
+        hash = md5(f"{data[0]}{n}".encode()).hexdigest()
+        if hash.startswith('00000'):
+            return n
---
+++ year2015/day04/part2.py
@@ -0,0 +1,10 @@
+from hashlib import md5
+
+
+def run(data: list[str], is_test: bool):
+    if is_test:
+        return True
+    for n in range(0, 10_000_000):
+        hash = md5(f"{data[0]}{n}".encode()).hexdigest()
+        if hash.startswith('000000'):
+            return n
