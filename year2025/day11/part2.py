a14d691eadf7e380b7e8fe2844d11b25904262c5
Tomas Zeman <tomaas.zeman@gmail.com>
2025-12-11 22:28:26 +0100

    2025 - Day 11 - change to cached DFS

--- year2025/day11/part2.py
+++ year2025/day11/part2.py
@@ -1,4 +1,5 @@
-from networkx import Graph, all_simple_paths
+from functools import cache
+from networkx import Graph
 from year2025.day11.common import parse_input
 from networkx.drawing.nx_agraph import to_agraph
 
@@ -9,15 +10,22 @@ def draw(graph: Graph):
     agraph.draw("graph.jpg")
 
 
+def count_paths(graph: Graph, source: str, destination: str):
+    @cache
+    def dfs(node):
+        if node == destination:
+            return 1
+        return sum([dfs(n) for n in graph.neighbors(node)])
+
+    return dfs(source)
+
+
 def run(data: list[str], is_test: bool):
     graph = parse_input(data)
-
-    # cutoffs are based on the drawing from `draw(graph)`
-    # TODO: if there's time, just change this to dfs() and slap @cache on it
     return (
-        len(list(all_simple_paths(graph, "svr", "fft", cutoff=10)))
-        * len(list(all_simple_paths(graph, "fft", "dac", cutoff=18)))
-        * len(list(all_simple_paths(graph, "dac", "out", cutoff=10)))
+        count_paths(graph, "svr", "fft")
+        * count_paths(graph, "fft", "dac")
+        * count_paths(graph, "dac", "out")
     )
 
 
