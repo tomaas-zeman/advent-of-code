85c8dbffc2817c9a0ed35a522f2e61a7097eb58b
Tomas Zeman <tomaas.zeman@gmail.com>
2025-12-13 16:46:14 +0100

    2025 - Day 10

--- year2025/day10/part2.py
+++ year2025/day10/part2.py
@@ -1,5 +1,42 @@
+import string
+from z3 import Int, Optimize
+from year2025.day10.common import parse_input
+
+
 def run(data: list[str], is_test: bool):
-    pass
+    inputs = parse_input(data)
+
+    total_presses = 0
+
+    for input in inputs:
+        buttons = [
+            Int(f"button_{c}") for c in string.ascii_letters[: len(input.buttons)]
+        ]
+
+        optimize = Optimize()
+
+        # only positive button presses
+        for button in buttons:
+            optimize.add(button >= 0)
+
+        # effects of each button press
+        for i in range(len(input.levels)):
+            expression = 0
+            for j in range(len(input.buttons)):
+                if i in input.buttons[j]:
+                    expression += buttons[j]
+            optimize.add(expression == input.levels[i])
+
+        # run the solver
+        optimize.minimize(sum(buttons))
+        optimize.check()
+
+        model = optimize.model()
+
+        for button in buttons:
+            total_presses += int(str(model[button]))
+
+    return total_presses
 
 
-test_result = None
+test_result = 33
